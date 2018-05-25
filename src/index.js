ca9f842e681a63b655b9f334073d9179b35fd172
mehadi-trackrep <mehadi541@gmail.com>
2018-05-25 17:48:30 +0600

    React Component Systems

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// 'let' diye variable mean kore ..
-let clock = function(){
-  return <h3>Now time is <h1>{new Date().toLocaleTimeString()}</h1></h3>
+
+function Cartoon(props){ // 'Cartoon' is a Component .. Condition: First letter Capital hobe; 'props' is properties
+  return <h1>Hello, {props.name} on {props.place}</h1>  // here, name and place are the properties ..
 }
 
-setInterval(function(){
-  ReactDOM.render(
-    clock(),
-    document.getElementById('root')
-  );
-},1000);
+function Show(){ // another Component
+  return <div>
+              <Cartoon name='Mehadi' place='Uniliver 5th floor'/>
+              <Cartoon name='Motaher' place='Tafadar 5th floor'/>
+         </div>
+}
+
+ReactDOM.render(
+  <Show />, // Component gula ke evabe likhte hoy ..
+  document.getElementById('root')
+);
