6f689f38dc61ff136c7d3b430946192a8d259500
mehadi-trackrep <mehadi541@gmail.com>
2018-05-26 12:41:45 +0600

    List items creating  in React

--- src/index.js
+++ src/index.js
@@ -2,36 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
-function Message(props){
-  if(props.Value){
-    return <h1>This is the first message :D</h1>
-  }else{
-    return <h1>This is the Second message from Mehadi</h1>
-  }
+const names = ['Mehadi', 'Hasan', 'Uzzal']
+
+function ListItems(props){
+  const values = props.items
+  const lists = values.map( (values,index) => <li key={index}>{values}</li> )  // 'index' is used for unique key of every listitems
+  return <ul>
+          {lists}
+         </ul>
 }
-
-class Switch_between_two_button extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {ck: true}
-  }
-
-  btnClick = () => {
-    this.setState({
-      ck: !this.state.ck
-    });
-  }
-
-  render(){
-    return <div>
-              <button onClick={this.btnClick}>Click to see message</button>
-              <Message Value={this.state.ck}/>
-            </div>
-  }
-}
-
 
 ReactDOM.render(
-  <Switch_between_two_button />,
+  <ListItems items={names}/>,
   document.getElementById('root')
 );
