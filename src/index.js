0d843585364f306408fc462b5f6a4155cc0a1671
mehadi-trackrep <mehadi541@gmail.com>
2018-05-26 13:53:46 +0600

    Handle Form Data

--- src/index.js
+++ src/index.js
@@ -1,18 +1,58 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// class FormList extends React.Component{
+//   constructor(props){
+//     super(props);
+//     this.state = {value:'initial_value'}
+//   }
+//
+//   render(){
+//     return <form>
+//             <select></select>
+//            </form>
+//   }
+// }
 
-const names = ['Mehadi', 'Hasan', 'Uzzal']
+class UIPrintChart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'PNG'
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handlePrint = this.handlePrint.bind(this);
+  }
 
-function ListItems(props){
-  const values = props.items
-  const lists = values.map( (values,index) => <li key={index}>{values}</li> )  // 'index' is used for unique key of every listitems
-  return <ul>
-          {lists}
-         </ul>
+  handlePrint() {
+    if (this.state.value) {
+      console.log(this.state.value);
+    }
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <select onChange={this.handleChange}>
+          <option value="PNG">PNG Image</option>
+          <option value="JPEG">JPEG Image</option>
+          <option value="PDF">PDF Document</option>
+          <option value="SVG">SVG Vector Image</option>
+        </select>
+        <button onClick={this.handlePrint}>Export Chart</button>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
-  <ListItems items={names}/>,
+  <UIPrintChart/>,
   document.getElementById('root')
 );
+
+
+// export default UIPrintChart;
